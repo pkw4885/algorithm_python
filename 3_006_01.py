278625682b3d2dd97e88898ff2713502b20ffb3e
pkw4885.github.io <69306556+pkw4885@users.noreply.github.com>
2022-01-24 21:25:04 +0900

    최대공약수 구하기

---
+++ 3_006_01.py
@@ -0,0 +1,11 @@
+# 최대공약수 구하기
+
+num1 = int(input('1보다 큰 정수 입력 : '))
+num2 = int(input('1보다 큰 정수 입력 : '))
+
+for i in range(1, (num1 + 1)) :
+    if num1 % i == 0 and num2 % i == 0:
+        print('공약수 : {}'.format(i))
+        maxNum = i
+
+print('최대공약수 : {}'.format(maxNum))
